adf7ce9603d0a705d4fe30de6eb41797f4ca8b11
kanxer <user.kanxer@gmail.com>
2024-06-23 12:30:59 +0530

    update2.0

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,18 @@
 // Navbar.js
+"use client";
 import Link from 'next/link';
 import Image from 'next/image';
-
+import { useState } from 'react';
 const Navbar = () => {
 
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    // Here you can apply theme-specific changes to your application
+    // For simplicity, we will just change the background color of the body
+    document.body.classList.toggle('clicked', newTheme === 'dark');
+  };
   return (
     <header className={`Flex `}>
       <nav className="flex justify-around items-center font-serif font-bold">
@@ -32,8 +41,8 @@ const Navbar = () => {
                 <a>About Me</a>
               </Link>
             </li>
-            <li className="cursor-pointer hover:cursor-pointer">
-              <Image src={`/moon.png`} alt="Language Icon" width={25} height={20} />
+            <li className="cursor-pointer hover:cursor-pointer" onClick={toggleTheme}>
+              <Image src={toggleTheme ? "/light.png" : "/moon.png"} alt="Language Icon" width={25} height={20} />
             </li>
           </ul>
         </div>
