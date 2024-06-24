f46af61ba1aadccb02628e01ec5c61e7112260c2
kanxer <user.kanxer@gmail.com>
2024-06-24 09:21:16 +0530

    update3.0

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@ module.exports = {
     images: {
       domains: ['www.ultraedit.com', 'https://www.ultraedit.com/'],
     },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   };
   
